51705cc536f133eb6fbc37f18f64d249a17ab21c
Mateo Jimenez <mateojimenez37@gmail.com>
2023-10-04 15:20:11 -0300

    import logo unveil

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,13 +2,13 @@ import React from 'react'
 import SearchSVG from '../SVGIcons/SearchSVG'
 import CartWidget from '../SVGIcons/CartWidget'
 import SignInSVG from '../SVGIcons/SignInSVG'
+import logo from '../../assets/unveil.png'
 
 const Navbar = () => {
   return (
     <div className='font-mono lg:bg-[#F8EFE0] flex items-center justify-around shadow-md w-full h-20 fixed top-0 left-0 min-[320px]:flex-wrap'>
        <div className='max-sm:block flex flex-shrink-0 min-[320px]:flex-shrink-1'>
-            <h1 className='font-sans font-bold text-2xl ml-10'>UNVEIL</h1>
-           <h3 className='flex items-center ml-3'>| for Sneakerheads</h3>
+           <img src={logo} alt='logo' className='h-8'/>
        </div>
        <div className="flex items-center gap-2">
         <input
---
+++ src/components/SVGIcons/LogoUnveil.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const LogoUnveil = () => {
+  return (
+    <div>
+        <img src='../../assets/unveil.png' alt='logo-unveil'/>
+    </div>
+  )
+}
+
+export default LogoUnveil
